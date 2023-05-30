cd00553cbdf9ed16dfd40e778e4e453ec10cab48
MusabBirhatYige <73779193+MusabBirhatYige@users.noreply.github.com>
2023-05-30 23:19:22 +0300

    Update reservation.js

--- Activity11_Widget/reservation.js
+++ Activity11_Widget/reservation.js
@@ -1,82 +1,76 @@
 $(document).ready(function() {
 	var emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
-	
-	// move the focus to the first text box
+
+	// Move the focus to the first text box
 	$("#arrival_date").focus();
-	
-	// the handler for the submit event of the form
-	// executed when the submit button is clicked
-	$("#reservation_form").submit(
-		function(event) {
-			var isValid = true;
-			
-			// validate the requested arrival date
-			var arrivalDate = $("#arrival_date").val().trim();
-			if (arrivalDate == "") {
-				$("#arrival_date").next().text("This field is required.");
-				isValid = false;
-			} else {
-				$("#arrival_date").next().text("");				
-			}
-			$("#arrival_date").val(arrivalDate);
-			
-			// validate the number of nights
-			var nights = $("#nights").val().trim();
-			if (nights == "") {
-				$("#nights").next().text("This field is required.");
-				isValid = false;
-			} else if (isNaN($("#nights").val())) {
-				$("#nights").next().text("This field must be numeric.");
-				isValid = false;
-			} else {
-				$("#nights").next().text("");
-			}
-			$("#nights").val(nights);		
 
-			// validate the name entry
-			var name = $("#name").val().trim();
-			if (name == "") {
-				$("#name").next().text("This field is required.");
-				isValid = false;
-			} 
-			else {
-				$("#name").val(name);
-				$("#name").next().text("");
-			}
+	// Handler for the submit event of the form
+	$("#reservation_form").submit(function(event) {
+		var isValid = true;
+
+		// Validate the requested arrival date
+		var arrivalDate = $("#arrival_date").val().trim();
+		if (arrivalDate === "") {
+			$("#arrival_date").next().text("This field is required.");
+			isValid = false;
+		} else {
+			$("#arrival_date").next().text("");
+		}
+		$("#arrival_date").val(arrivalDate);
+
+		// Validate the number of nights
+		var nights = $("#nights").val().trim();
+		if (nights === "") {
+			$("#nights").next().text("This field is required.");
+			isValid = false;
+		} else if (isNaN(nights)) {
+			$("#nights").next().text("This field must be numeric.");
+			isValid = false;
+		} else {
+			$("#nights").next().text("");
+		}
+		$("#nights").val(nights);
+
+		// Validate the name entry
+		var name = $("#name").val().trim();
+		if (name === "") {
+			$("#name").next().text("This field is required.");
+			isValid = false;
+		} else {
 			$("#name").val(name);
-						
-			// validate the email entry with a regular expression
-			var email = $("#email").val();
-			if (email == "") { 
-				$("#email").next().text("This field is required.");
-				isValid = false;
-			} else if ( !emailPattern.test(email) ) {
-				$("#email").next().text("Must be a valid email address.");
-				isValid = false;
-			} else {
-				$("#email").next().text("");
-			}
-			$("#email").val(email); 
-			
-			// validate the phone number
-			var phone = $("#phone").val().trim();
-			if (phone == "") { 
-				$("#phone").next().text("This field is required.");
-				isValid = false; 
-			} else {
-				$("#phone").next().text("");
-			}
-			$("#phone").val(phone);
-			
-			// prevent the submission of the form if any entries are invalid 
-			if (isValid == false) {
-				event.preventDefault();				
-			}
-		} // end function
-	);	// end submit
-}); // end ready
+			$("#name").next().text("");
+		}
+		$("#name").val(name);
 
-$(document).ready(function() {
+		// Validate the email entry with a regular expression
+		var email = $("#email").val().trim();
+		if (email === "") {
+			$("#email").next().text("This field is required.");
+			isValid = false;
+		} else if (!emailPattern.test(email)) {
+			$("#email").next().text("Must be a valid email address.");
+			isValid = false;
+		} else {
+			$("#email").next().text("");
+		}
+		$("#email").val(email);
+
+		// Validate the phone number
+		var phone = $("#phone").val().trim();
+		if (phone === "") {
+			$("#phone").next().text("This field is required.");
+			isValid = false;
+		} else {
+			$("#phone").next().text("");
+		}
+		$("#phone").val(phone);
+
+		// Prevent the submission of the form if any entries are invalid
+		if (!isValid) {
+			event.preventDefault();
+		}
+	});
+
 	$('.tabs ul li a').click(function() {
 		var tabId = $(this).attr('data-tab');
 		$('.tabs ul li a').removeClass('active');
@@ -84,9 +78,7 @@ $(document).ready(function() {
 		$(this).addClass('active');
 		$('#' + tabId).addClass('active');
 	});
-});
 
-$(document).ready(function() {
 	var currentDate = new Date();
 	var maxDate = new Date();
 	maxDate.setDate(currentDate.getDate() + 90);
@@ -99,10 +91,7 @@ $(document).ready(function() {
 			$("#arrival_date").val(selectedDate);
 		}
 	});
-});
 
-
-$(document).ready(function() {
 	$("#policies").click(function() {
 		$("#dialog").dialog({
 			modal: true,
@@ -115,5 +104,3 @@ $(document).ready(function() {
 		});
 	});
 });
-
-
